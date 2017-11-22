6252b1c2d1399e705a74d3a07cbb83d6bf31c8c4
Lawrence McDaniel <lpm0073@gmail.com>
2017-11-22 12:23:33 -0600

    setup Cognito, DynamoDB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,15 @@ export const environment = {
 
     region: 'us-east-1',
 
-    identityPoolId: 'us-east-1:eb41c502-754c-479d-9619-fcdcc973273f',
-    userPoolId: 'us-east-1_77J4NlPVl',
-    clientId: 'm3s529udo8rgn3317ad2a66t0',
+    identityPoolId: 'us-east-1:91a16994-6330-4bfc-8b13-c3bedbb327b7',
+    userPoolId: 'us-east-1_jjPFmgYHq',
+    clientId: '3peakb2jh4kcbo70dk48cnc4su',
 
     rekognitionBucket: 'rekognition-pics',
     albumName: "usercontent",
     bucketRegion: 'us-east-1',
 
-    ddbTableName: 'LoginTrailfatbrandsdataloader',
+    ddbTableName: 'LoginTraildataloader',
 
     cognito_idp_endpoint: '',
     cognito_identity_endpoint: '',
