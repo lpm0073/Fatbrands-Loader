862fa962b75ea7943bb0629def7f3426b513765e
Lawrence McDaniel <lpm0073@gmail.com>
2017-11-22 11:50:07 -0600

    integrated to AWS Cognito, DynamoDB

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,21 +3,20 @@ export const environment = {
 
     region: 'us-east-1',
 
-    identityPoolId: 'us-east-1:fbe0340f-9ffc-4449-a935-bb6a6661fd53',
-    userPoolId: 'us-east-1_PGSbCVZ7S',
-    clientId: 'hh5ibv67so0qukt55c5ulaltk',
+    identityPoolId: 'us-east-1:eb41c502-754c-479d-9619-fcdcc973273f',
+    userPoolId: 'us-east-1_77J4NlPVl',
+    clientId: 'm3s529udo8rgn3317ad2a66t0',
 
     rekognitionBucket: 'rekognition-pics',
     albumName: "usercontent",
     bucketRegion: 'us-east-1',
 
-    ddbTableName: 'LoginTrail',
+    ddbTableName: 'LoginTrailfatbrandsdataloader',
 
     cognito_idp_endpoint: '',
     cognito_identity_endpoint: '',
     sts_endpoint: '',
     dynamodb_endpoint: '',
     s3_endpoint: ''
-
 };
 
